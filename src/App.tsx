281d826f4bf3fd65acb9e11999c689ac366afa97
MAC <user@MACs-MacBook-Pro.local>
2024-08-30 07:46:33 +0100

    food order

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,24 @@ import FoodOrderForm from "./Components/FoodOrder";
 import { useState } from "react";
 
 function App() {
-  const [order, setOrder] = useState("");
+  const [order, setOrder] = useState<string>("");
+
   const handleAddItemToOrder = (itemName: string) => {
-    itemName;
+    // Update the order state with the new item
+    setOrder((prevOrder) =>
+      prevOrder ? `${prevOrder}, ${itemName}` : itemName
+    );
   };
+
   return (
     <div className={styles.container}>
+      <h1>Food Order</h1>
       <FoodItems
         name="Shashuka"
         price={5.99}
         imgSrc="https://images.unsplash.com/photo-1590412200988-a436970781fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        addItemToOrder={handleAddItemToOrder}></FoodItems>
+        addItemToOrder={handleAddItemToOrder}
+      />
       <FoodItems
         name="Spaghetti Carbonara"
         price={6.99}
@@ -27,7 +34,7 @@ function App() {
         imgSrc="https://images.unsplash.com/photo-1589187151053-5ec8818e661b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
         addItemToOrder={handleAddItemToOrder}
       />
-      <FoodOrderForm initialOrder={order}></FoodOrderForm>
+      <FoodOrderForm initialOrder={order} />
     </div>
   );
 }
